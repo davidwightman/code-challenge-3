fc951d69abb3a2a7e4f32ed812d468638d48d92f
wightman <davidwightmanmusic@gmail.com>
2017-02-15 23:20:01 -0500

    initial commit

---
+++ library.js
@@ -0,0 +1,48 @@
+/*Code Challenge
+We're also asking people to submit a quick code sample for us to review. We're after an ES5 Javascript class for a library which allows people to find book details by ISBN number and search for books by their title. Please add comments/thoughts about any limitations your implementations yours had and why you chose to do things a certain way. This shouldn't take longer than 15 minutes.*/
+
+
+//constructor
+function Library(books) {
+	this._books = books 
+	//create lookup object by isbn
+	this._byIsbn = {};
+	for(var i=0; i < books.length; i++){
+		this._byIsbn[books[i].isbn] = books[i]
+	}
+}
+//search by isbn method
+Library.prototype.searchByIsbn =function(isbn) {
+	return this._byIsbn[isbn] || null;
+}
+// search by tite method
+Library.prototype.searchByTitle = function(title) {
+	//to lowercase to normalize the query
+	title = title.toLowerCase()
+	for(var i=0; i < this._books.length; i++){
+		//search for portion of title
+		if (this._books[i].title.toLowerCase().indexOf(title) !== -1){
+			return this._books[i]
+		}
+	}
+	return null
+}
+//constructing an instance of library
+var obj = new Library([
+	{
+		isbn: 11,
+		author: "name",
+		title: "nameOfBook1"
+	}, {
+		isbn: 23,
+		author: "name2",
+		title: "nameOfBook2"
+	}]);
+//invoking obj's Library method
+console.log(obj.searchByIsbn(11))
+
+console.log(obj.searchByTitle("Book2"))
+
+//problems: 
+//1. exact title matches
+//2. finds the first title match. does not find all title matches
